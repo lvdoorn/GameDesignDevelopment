9e90abf44de87018e0bf08348a215c97034308b9
PASTADWINDOWS\pastad <stadlbauerpascal@gmail.com>
2016-11-19 14:09:31 +0100

    added PlayerController (testSprite)

---
+++ SurvivalOfThe/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using NDream.AirConsole;
+using Newtonsoft.Json.Linq;
+
+public class PlayerScript : MonoBehaviour
+{
+
+  private Rigidbody2D rb_;
+  private bool moved_;
+  private Animator anim_;
+  private int id_;
+  private string movement_;
+  public bool has_focus_;
+
+  // Use this for initialization
+  void Start ()
+  {
+    rb_ = GetComponent<Rigidbody2D>();
+    moved_ = false;
+    anim_ = GetComponent<Animator>();
+    anim_.enabled = false;
+    movement_ = "S";
+    has_focus_ = false;
+
+    AirConsole.instance.onMessage += OnMessage;
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    bool m = false;
+    if (movement_ == "R")
+    {
+      rb_.AddForce(new Vector2(5, 0));
+      m = true;
+    }
+    if (movement_ == "L")
+    {
+      rb_.AddForce(new Vector2(-5, 0));
+      m = true;
+    }
+    if (movement_ == "U")
+    {
+      rb_.AddForce(new Vector2(0, 5));
+      m = true;
+    }
+    if (movement_ == "D")
+    {
+      rb_.AddForce(new Vector2(0, -5));
+      m = true;
+    }
+
+
+    if (has_focus_)
+    {
+      Camera cam = GameObject.Find("MainCamera").GetComponent<Camera>();
+      Vector3 p = GameObject.Find("Player").transform.position;
+      cam.transform.position = new Vector3(p.x, p.y, -10);// + new Vector2(50,50) ;
+    }
+
+    if (m && ( !moved_ ))
+    {
+      anim_.enabled = true;
+    }
+    if(!m && moved_)
+    {
+      anim_.enabled = false;
+    }
+    moved_ = m;
+
+  }
+
+  void OnMessage(int from, JToken data)
+  {
+    Debug.Log((string)data["direction"]);
+    if(from == id_)
+    {
+      string dir = (string)data["direction"];
+      movement_ = dir;
+    }
+
+  }
+
+  public void setId(int id)
+  {
+    id_ = id;
+  }
+  public int getId()
+  {
+    return id_;
+  }
+}
+ 
---
+++ SurvivalOfThe/Assets/Scripts/PlayersScript.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+using NDream.AirConsole;
+using Newtonsoft.Json.Linq;
+
+public class PlayersScript : MonoBehaviour
+{
+  private List<GameObject> players_;
+
+  public GameObject prefab;
+
+  // Use this for initialization
+  void Start()
+  {
+    int player_count = 3;
+    players_ = new List<GameObject>();
+
+    AirConsole.instance.onConnect += OnConnect;
+    AirConsole.instance.onDisconnect += OnDisconnect;   
+    
+    SpriteRenderer sr = prefab.GetComponent<SpriteRenderer>();
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+
+  void AddPlayer(int id)
+  {
+    GameObject clone = Instantiate(prefab, prefab.transform.position, prefab.transform.rotation) as GameObject;
+    Debug.Log("inst");
+
+    clone.GetComponent<SpriteRenderer>().enabled = true;
+    clone.GetComponent<PlayerScript>().enabled = true;
+    clone.GetComponent<PlayerScript>().setId(id);
+
+    players_.Add(clone);
+
+    SetFocus(id);
+  }
+  void RemovePlayer(int id)
+  {
+  
+    for (int i = players_.Count - 1; i > -1; i--)
+    {
+      PlayerScript ps = players_[i].GetComponent<PlayerScript>();
+      if (ps.getId() ==  id)
+      {
+        Destroy(players_[i] );
+        players_.RemoveAt(i);
+      }
+    }
+  }
+
+  void OnConnect(int device_id)
+  {
+    AddPlayer(device_id);
+  }
+
+  void OnDisconnect(int device_id)
+  {
+    RemovePlayer(device_id);
+  }
+  void SetFocus(int id)
+  {
+    for (int i = players_.Count - 1; i > -1; i--)
+    {
+      PlayerScript ps = players_[i].GetComponent<PlayerScript>();
+      if (ps.getId() == id)
+        ps.has_focus_ = true;
+      else
+        ps.has_focus_ = false;
+
+    }
+  }
+}
